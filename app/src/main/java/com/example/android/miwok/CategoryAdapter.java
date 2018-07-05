be41912642bf6395bd79c8ca71275bfc50f31fe4
AF_BA <amypyfong@gmail.com>
2018-07-04 22:47:02 -0700

    fix styles.xml for the tab

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -29,8 +30,12 @@ import com.example.android.miwok.PhrasesFragment;
  */
 public class CategoryAdapter extends FragmentPagerAdapter {
 
-    public CategoryAdapter(FragmentManager fm) {
+    /** Context of the app */
+    private Context mContext;
+
+    public CategoryAdapter(Context context, FragmentManager fm) {
         super(fm);
+        mContext = context;
     }
 
     @Override
@@ -49,5 +54,18 @@ public class CategoryAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return mContext.getString(R.string.category_numbers);
+        } else if (position == 1) {
+            return mContext.getString(R.string.category_family);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_colors);
+        } else {
+            return mContext.getString(R.string.category_phrases);
+        }
     }
 }
